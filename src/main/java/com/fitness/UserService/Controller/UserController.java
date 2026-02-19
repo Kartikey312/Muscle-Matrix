8728331ab3232255b4cf50742c8a761aaddc5806
kartikeymishra312 <kartikmishraharsh@gmail.com>
2026-02-19 13:18:07 +0530

    valid method

--- src/main/java/com/fitness/UserService/Controller/UserController.java
+++ src/main/java/com/fitness/UserService/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.fitness.UserService.Controller;
 import com.fitness.UserService.Services.UserService;
 import com.fitness.UserService.dto.RegisterRequest;
 import com.fitness.UserService.dto.UserResponse;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponse> register(@RequestBody RegisterRequest request ){
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request ){
         return ResponseEntity.ok(userService.register(request));
 
     }
