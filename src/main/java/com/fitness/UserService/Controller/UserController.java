e09983b6bbd6e749aaf63b2d713d458002cbcff0
kartikeymishra312 <kartikmishraharsh@gmail.com>
2026-02-19 13:36:52 +0530

    get_controller

--- src/main/java/com/fitness/UserService/Controller/UserController.java
+++ src/main/java/com/fitness/UserService/Controller/UserController.java
@@ -6,16 +6,21 @@ import com.fitness.UserService.dto.UserResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 @AllArgsConstructor
 public class UserController {
     private UserService userService;
+
+    @GetMapping("/{userID}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable String userID){
+        return ResponseEntity.ok(userService.getUserProfile(userID));
+    }
+
+
+
 
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request ){
