fe73d2b563ae873fc8c5904f1cde756a12356714
kartikeymishra312 <kartikmishraharsh@gmail.com>
2026-02-28 13:33:51 +0530

    changes

--- activityService/src/main/java/com/example/activityService/config/WebClientConfig.java
+++ activityService/src/main/java/com/example/activityService/config/WebClientConfig.java
@@ -2,8 +2,10 @@ package com.example.activityService.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Configuration
 public class WebClientConfig {
 
     @Bean
@@ -13,9 +15,9 @@ public class WebClientConfig {
     }
 
     @Bean
-    public WebClient userServiceWebClient(WebClient.Builder webClientBuilder) {
-        return webClientBuilder
-                .baseUrl("http://USER-SERVICE")
+    public WebClient userServiceWebClient(WebClient.Builder builder) {
+        return builder
+                .baseUrl("http://USERSERVICE")
                 .build();
     }
 }
