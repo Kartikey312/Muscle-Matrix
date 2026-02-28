eeb3e4e8103a45b589799bfc30e1f95522d55b66
kartikeymishra312 <kartikmishraharsh@gmail.com>
2026-02-28 12:53:45 +0530

    changes in module

---
+++ userService/src/main/java/com/fitness/UserService/Controller/UserController.java
@@ -0,0 +1,38 @@
+package com.fitness.UserService.Controller;
+
+import com.fitness.UserService.Services.UserService;
+import com.fitness.UserService.dto.RegisterRequest;
+import com.fitness.UserService.dto.UserResponse;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@AllArgsConstructor
+public class UserController {
+    private UserService userService;
+
+    @GetMapping("/{userID}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable String userID){
+        return ResponseEntity.ok(userService.getUserProfile(userID));
+    }
+
+
+
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request ){
+        return ResponseEntity.ok(userService.register(request));
+
+    }
+
+    @GetMapping("/{userID}/validate")
+    public ResponseEntity<Boolean> validateUser(@PathVariable String userID){
+        return ResponseEntity.ok(userService.existbyUserId(userID));
+    }
+
+
+}
+
