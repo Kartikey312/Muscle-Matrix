390c59644ea989ca3018b477e3eebccd3c53c8f8
kartikeymishra312 <kartikmishraharsh@gmail.com>
2026-02-28 12:52:14 +0530

    changes in module

---
+++ activityService/src/main/java/com/example/activityService/config/WebClientConfig.java
@@ -0,0 +1,21 @@
+package com.example.activityService.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class WebClientConfig {
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    public WebClient userServiceWebClient(WebClient.Builder webClientBuilder) {
+        return webClientBuilder
+                .baseUrl("http://USER-SERVICE")
+                .build();
+    }
+}
--- src/main/java/com/fitness/UserService/models/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.fitness.UserService.models;
-
-public enum UserRole {
-    USER,ADMIN
-}
